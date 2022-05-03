9d29eaa5577e6fefabceed998ae981ef3acc57af
sangho.lee <sangho.lee@z-emotion.com>
2022-05-03 19:45:38 +0900

    add docker env.

--- next.config.js
+++ next.config.js
@@ -1,15 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
 module.exports = {
   nextConfig,
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    })
-    return config
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+  experimental: {
+    outputStandalone: true,
   },
 };
