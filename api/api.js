d4827fc8c32f54e225ae798cd4dc7ccc17931e04
no-ah-song <noah.song@z-emotion.com>
2022-05-11 15:50:11 +0900

    api refactoring

--- api/api.js
+++ api/api.js
@@ -1,4 +1,4 @@
-import _ from 'lodash';
+import _, { forEach } from 'lodash';
 
 const getBrandList = async () => {
   const res = await fetch('https://zfit-data.s3.ap-northeast-2.amazonaws.com/data/brands.json');
@@ -47,40 +47,40 @@ const getFittingImages = async ({
     try {
       if (brandId && productId && color && size && gender && height) {
         let fittings = data.fitting[brandId][productId][color][size][gender];
-        let nearestHeight = await processBodyInfoData(fittings, 'height', height);
-        fittings = await fittings.filter(fitting => {
+        let nearestHeight = processBodyInfoData(fittings, 'height', height);
+        fittings = fittings.filter(fitting => {
           return fitting.height == nearestHeight;
         });
-        let nearestHip = await processBodyInfoData(fittings, 'hip', hip);
-        fittings = await fittings.filter(fitting => {
+        let nearestHip = processBodyInfoData(fittings, 'hip', hip);
+        fittings = fittings.filter(fitting => {
           return fitting.hip == nearestHip;
         });
-        let nearestArmLength = await processBodyInfoData(fittings, 'armLength', armLength);
-        fittings = await fittings.filter(fitting => {
+        let nearestArmLength = processBodyInfoData(fittings, 'armLength', armLength);
+        fittings = fittings.filter(fitting => {
           return fitting.armLength == nearestArmLength;
         });
-        let nearestInseam = await processBodyInfoData(fittings, 'inseam', inseam);
-        fittings = await fittings.filter(fitting => {
+        let nearestInseam = processBodyInfoData(fittings, 'inseam', inseam);
+        fittings = fittings.filter(fitting => {
           return fitting.inseam == nearestInseam;
         });
-        let nearestChest = await processBodyInfoData(fittings, 'chest', chest);
-        fittings = await fittings.filter(fitting => {
+        let nearestChest = processBodyInfoData(fittings, 'chest', chest);
+        fittings = fittings.filter(fitting => {
           return fitting.chest == nearestChest;
         });
-        let nearestNeck = await processBodyInfoData(fittings, 'neck', neck);
-        fittings = await fittings.filter(fitting => {
+        let nearestNeck = processBodyInfoData(fittings, 'neck', neck);
+        fittings = fittings.filter(fitting => {
           return fitting.neck == nearestNeck;
         });
-        let nearestBelt = await processBodyInfoData(fittings, 'belt', belt);
-        fittings = await fittings.filter(fitting => {
+        let nearestBelt = processBodyInfoData(fittings, 'belt', belt);
+        fittings = fittings.filter(fitting => {
           return fitting.belt == nearestBelt;
         });
-        let nearestWaist = await processBodyInfoData(fittings, 'waist', waist);
-        fittings = await fittings.filter(fitting => {
+        let nearestWaist = processBodyInfoData(fittings, 'waist', waist);
+        fittings = fittings.filter(fitting => {
           return fitting.waist == nearestWaist;
         });
-        let nearestShoulder = await processBodyInfoData(fittings, 'acrossShoulder', acrossShoulder);
-        fittings = await fittings.filter(fitting => {
+        let nearestShoulder = processBodyInfoData(fittings, 'acrossShoulder', acrossShoulder);
+        fittings = fittings.filter(fitting => {
           return fitting.acrossShoulder == nearestShoulder;
         });
         return fittings[0];
@@ -95,29 +95,28 @@ const getFittingImages = async ({
   return fittingImage;
 };
 
-async function findNearestInteger(data, target) {
+function findNearestInteger(data, target) {
   let near = 0;
   let abs = 0;
   let min = 999; // 해당 범위에서 가장 큰 값
 
-  for (var i = 0; i < data.length; i++) {
-    abs = data[i] - target < 0 ? -(data[i] - target) : data[i] - target;
+  data.forEach(el => {
+    abs = el - target < 0 ? -(el - target) : el - target;
     if (abs < min) {
       min = abs;
-      near = data[i];
+      near = el;
     }
-  }
-
+  });
   return near;
 }
 
-async function processBodyInfoData(data, key, value) {
-  await data.sort((a, b) => a[key] - b[key]);
+function processBodyInfoData(data, key, value) {
+  data.sort((a, b) => a[key] - b[key]);
   let distinctArr = _.uniqBy(data, key);
   const keyArr = distinctArr.map(el => {
     return el[key];
   });
-  let nearestKey = await findNearestInteger(keyArr, value);
+  let nearestKey = findNearestInteger(keyArr, value);
   return nearestKey;
 }
 export { getBrandList, getFittingImages };
